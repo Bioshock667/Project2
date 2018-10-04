9d9567ca98bc286576deadc9e36a8eb9b024ef1b
bss1251 <43614982+bss1251@users.noreply.github.com>
2018-10-04 13:58:37 -0400

    Update TestingSerlvet.java

--- TestingServlet/TestingServer/src/main/java/serlvet/TestingSerlvet.java
+++ TestingServlet/TestingServer/src/main/java/serlvet/TestingSerlvet.java
@@ -23,7 +23,7 @@ public class TestingSerlvet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Origin", "http://localhost:4200/*");
         response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
 		response.getWriter().append("{\"mainData\":\"testingOne\"}");
 	}
