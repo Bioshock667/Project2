bca875981e8973c0632234c0135989a5220f5e53
unknown <sware128@gmail.com>
2018-10-05 15:19:04 -0400

    more testing testing

--- TestingServlet/TestingServer/src/main/java/tests/BasicTest.java
+++ TestingServlet/TestingServer/src/main/java/tests/BasicTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import pages.LoginPage;
+
 public class BasicTest {
 	
 	@Test
@@ -15,7 +17,13 @@ public class BasicTest {
 		System.setProperty("webdriver.chrome.driver",f.getAbsolutePath());
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://dev-caliber.revature.tech/");
+		LoginPage lp = new LoginPage(driver);
+		lp.getUName().sendKeys("calibot@revature.com");
+		lp.getPwd().sendKeys("*6Ak4-&kXnNTfTh6");
+		lp.getLogin().click();
+		
 		driver.quit();
+		Assert.assertEquals(1, 1);
 	}
 
 }
