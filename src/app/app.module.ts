39fa9fc4c30a2fce52f611cf8683d9de8fa1c24b
unknown <sware128@gmail.com>
2018-10-08 13:12:09 -0400

    updating angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule} from '@angular/material/card'
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    MatCardModule,
     MatProgressSpinnerModule
   ],
   providers: [],
