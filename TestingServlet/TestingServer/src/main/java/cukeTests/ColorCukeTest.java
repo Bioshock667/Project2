5e43452ba4043fc896ca623351b9ae88d689e137
unknown <sware128@gmail.com>
2018-10-10 11:30:27 -0400

    updating cuke tests

--- TestingServlet/TestingServer/src/main/java/cukeTests/ColorCukeTest.java
+++ TestingServlet/TestingServer/src/main/java/cukeTests/ColorCukeTest.java
@@ -13,7 +13,7 @@ import pages.NavBarPage;
 
 @CucumberOptions(
         features = "../webapps/TestingServer/WEB-INF/classes/features/colorfaces.feature",
-        glue = {"cukeSteps"})
+        glue = {"cukeSteps.FaceClickingSteps"})
 public class ColorCukeTest {
 	private TestNGCucumberRunner testNGCucumberRunner;
     @BeforeClass(alwaysRun = true)
