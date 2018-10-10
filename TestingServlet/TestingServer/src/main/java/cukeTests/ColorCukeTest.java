89d3b874446ca71bf587ad0057f340f184c4c787
unknown <sware128@gmail.com>
2018-10-10 09:10:00 -0400

    color clicking face tests

---
+++ TestingServlet/TestingServer/src/main/java/cukeTests/ColorCukeTest.java
@@ -0,0 +1,38 @@
+package cukeTests;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import pages.NavBarPage;
+
+@CucumberOptions(
+        features = "src/main/resources/features/colorfaces.feature",
+        glue = {"cukeSteps"})
+public class ColorCukeTest {
+	private TestNGCucumberRunner testNGCucumberRunner;
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+ 
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+ 
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideFeatures();
+    }
+ 
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+    }
+}
